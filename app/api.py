fc70bf74eda10455b0196bc74e706daba5f1fb48
Ellen Coutinho <ellencls@al.insper.edu.br>
2024-10-29 11:57:01 -0300

    Integração ao Postgres por docker-compose

--- app/api.py
+++ app/api.py
@@ -4,7 +4,7 @@ from scrapping import get_info
 from typing import Annotated
 from hashlib import sha256
 from dotenv import load_dotenv
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 from sqlmodel import SQLModel, Session, Field, select
 import jwt
 import os
@@ -33,15 +33,9 @@ class Login(SQLModel):
     email: str = Field(...)
     senha: str = Field(...)
 
-engine = create_engine(f"mysql+pymysql://{user}:{password}@{host}/", pool_pre_ping=True)
+engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}/{database}")
 
-with engine.connect() as connection:
-    connection.execution_options(autocommit=True)
-    connection.execute(text(f"CREATE DATABASE IF NOT EXISTS cloud"))
-
-engine = create_engine(f"mysql+pymysql://{user}:{password}@{host}/{database}")
-
-SQLModel.metadata.create_all(engine)  # Recria todas as tabelas
+SQLModel.metadata.create_all(engine)
 
 def get_session():
     with Session(engine) as session:
@@ -68,8 +62,8 @@ def registro(user: UserCreate, session: SessionDep):
 
     # Criar token JWT
     token = jwt.encode(
-        payload=novo_usuario.nome,
-        key=os.getenv('MINHA_SENHA')
+        payload={"nome":novo_usuario.nome},
+        key=str(os.getenv('MINHA_SENHA'))
     )
     return {"token": token}
 
@@ -83,15 +77,15 @@ def login(login: Login, session: SessionDep):
 
     # Criar token JWT
     token = jwt.encode(
-        payload=usuario.nome,
-        key=os.getenv('MINHA_SENHA')
+        payload={"nome":usuario.nome},
+        key=str(os.getenv('MINHA_SENHA'))
     )
     return {"token": token}
 
 def validar_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
     token = credentials.credentials
     try:
-        payload = jwt.decode(token, key=os.getenv('MINHA_SENHA'), algorithms=["HS256"])
+        payload = jwt.decode(token, key=str(os.getenv('MINHA_SENHA')), algorithms=["HS256"])
         return payload
     except:
         raise HTTPException(status_code=403, detail="Token ausente ou inválido")
@@ -100,5 +94,3 @@ def validar_token(credentials: HTTPAuthorizationCredentials = Depends(security))
 def consulta(user_info: dict = Depends(validar_token)):
     info = get_info()
     return info
-
-    
