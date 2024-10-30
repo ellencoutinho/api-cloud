649482163aa02381224fb9ffd29c444d99102e47
Ellen Coutinho <ellencls@al.insper.edu.br>
2024-10-29 22:56:14 -0300

    Atualiza data do webscrapping para horário de brasília

--- app/scrapping.py
+++ app/scrapping.py
@@ -1,14 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
-from datetime import date
-
+from datetime import datetime, timedelta, date
 
 def get_info():
     response = requests.get('https://g1.globo.com/previsao-do-tempo/sp/sao-paulo.ghtml')
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
         return {
-            "data": str(date.today()),
+            "data": str((datetime.utcnow() - timedelta(hours=3)).date()),
             "descricao": soup.find(class_='forecast-header__summary').text,
             "temp_maxima": soup.find(class_='forecast-today__temperature forecast-today__temperature--max').text[:-4],
             "temp_minima": soup.find(class_='forecast-today__temperature forecast-today__temperature--min').text[:-4]
